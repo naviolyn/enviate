1ad7eaeb77edda7ed456871d679fc853e5ce9074
naviolyn <artemisathena456@gmail.com>
2025-01-16 09:39:18 +0700

    route

--- enviate/routes/web.php
+++ enviate/routes/web.php
@@ -6,9 +6,9 @@ use App\Livewire\TodayTask;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('livewire.today-task');
+// });
 Route::get('/login', function () {
     return view('login');
 });
@@ -19,4 +19,4 @@ Route::get('/register', function () {
 
 Route::get('/today-task', TodayTask::class);
 Route::get('/challenge', Challenge::class);
-
+Route::get('/', TodayTask::class);
