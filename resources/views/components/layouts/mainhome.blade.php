096d59a191deb12bfb1000f18938cbdb3f614112
naviolyn <artemisathena456@gmail.com>
2025-01-24 12:18:06 +0700

    fix navabar and mainhome

--- resources/views/components/layouts/mainhome.blade.php
+++ resources/views/components/layouts/mainhome.blade.php
@@ -16,12 +16,58 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
+    <div class="background"></div>
     <div class="h-screen flex flex-col">
         @include('components.navbar')
         <div class="h-full">
             @yield('content')
         </div>
-    </div>         
+    </div>        
 
 </body>
 </html>
+
+<style>
+    html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box; /* Standardize box model */
+    overflow-x: hidden;
+}
+    .background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 110%;
+        height: 100%;
+        background: 
+        url('{{ asset('img/bg.jpg') }}');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        filter: blur(3px); /* Only blur the background */
+        z-index: -1; /* Send it behind everything */
+        margin: -8px;
+        overflow: hidden;
+    }
+    .background::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.6); /* Darken effect */
+        z-index: 1;
+    }
+    /* Content styles */
+    .content {
+        position: relative; /* Keep content unaffected by background styling */
+        z-index: 2; /* Ensure content appears above background */
+        color: white;
+        font-size: 2rem;
+        text-align: center;
+    }
+</style>
