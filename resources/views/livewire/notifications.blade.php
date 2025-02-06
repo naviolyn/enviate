114a56267353588340c64a3c5a0479b9599b6c1f
finorosatabitha <finorosatabitha2118@gmail.com>
2025-02-06 15:34:16 +0700

    notif date

--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -9,7 +9,7 @@
                 <p class="text-white">
                     {{ $notification->message }}
                     <br>
-                    <span class="text-xs text-gray-400">Received on: {{ $notification->created_at->format('d-m-Y H:i') }}</span>
+                    <span class="text-xs text-gray-400">Received on: {{ $notification->created_at->format('d-m-Y') }}</span>
                 </p>
             </li>
             @endforeach
